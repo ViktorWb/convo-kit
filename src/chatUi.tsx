47446c87353433bc2b37d8afafd44ce046c240b4
Viktor Westberg <viktor.westberg@hotmail.com>
2025-11-13 14:08:03 +0100

    Create separate ScrollBox component for more generic usage

--- src/chatUi.tsx
+++ src/chatUi.tsx
@@ -1,27 +1,16 @@
-import React, { ComponentProps, useEffect, useMemo, useRef } from 'react'
+import React, { ComponentProps, useCallback, useEffect, useMemo, useRef } from 'react'
 import { groupMessages } from './groupMessages'
 import { ChatMessageContent, ChatMessage, ToolCall, ToolDefinition } from './types'
 
-export function ChatUi<T extends readonly ToolDefinition[] = readonly ToolDefinition[]>(
+export function ScrollBox(
     props: {
-        messages: ChatMessage<T>[]
-        UserMessageComponent: React.ComponentType<{ messageIndex: number; groupedMessages: ChatMessage<T>[]; content: ChatMessageContent }>
-        AssistantMessageComponent: React.ComponentType<{
-            messageIndex: number
-            groupedMessages: ChatMessage<T>[]
-            text: string
-            onContentShow: (text: string) => void
-        }>
         scrollContainer?: () => HTMLElement
-        ToolCallComponent: React.ComponentType<{ messageIndex: number; groupedMessages: ChatMessage<T>[]; toolCall: ToolCall<T> }>
-        footer?: React.ReactNode
-    } & ComponentProps<'div'>
+        renderContent: (onExpanded: () => void) => React.ReactNode
+    } & Omit<ComponentProps<'div'>, 'children'>
 ) {
-    const { messages, scrollContainer, UserMessageComponent, AssistantMessageComponent, ToolCallComponent, footer, ...rest } = props
+    const { scrollContainer, ...rest } = props
 
     const chatUiRef = useRef<HTMLDivElement>(null)
-
-    const groupedMessages = useMemo(() => groupMessages(props.messages), [props.messages])
 
     const scrollIsBottomRef = useRef(true)
     const lastShowContentTimestampRef = useRef(0)
@@ -83,41 +72,76 @@ export function ChatUi<T extends readonly ToolDefinition[] = readonly ToolDefini
 
     useEffect(() => {
         scrollToBottomIfNeeded()
-    }, [groupedMessages])
+    }, [props.renderContent])
 
     return (
         <div ref={chatUiRef} {...rest}>
-            {groupedMessages
-                .filter((msg) => msg.msg.role !== 'system' && msg.msg.role !== 'toolResponse')
-                .map(({ indexes, msg }: { indexes: number[]; msg: Exclude<ChatMessage<T>, { role: 'system' | 'toolResponse' }> }, i, arr) => {
-                    const key = indexes[0]
-
-                    if (msg.role === 'user') {
-                        return (
-                            <React.Fragment key={key}>
-                                {msg.content.map((x, i2) => (
-                                    <props.UserMessageComponent key={i2} messageIndex={i} groupedMessages={arr.map((x) => x.msg)} content={x} />
-                                ))}
-                            </React.Fragment>
-                        )
-                    }
-                    if (msg.role === 'assistant') {
-                        return msg.content === '' ? null : (
-                            <props.AssistantMessageComponent
-                                key={key}
-                                messageIndex={i}
-                                groupedMessages={arr.map((x) => x.msg)}
-                                text={msg.content}
-                                onContentShow={() => {
-                                    lastShowContentTimestampRef.current = Date.now()
-                                    scrollToBottomIfNeeded()
-                                }}
-                            />
-                        )
-                    }
-                    return <props.ToolCallComponent key={key} messageIndex={i} groupedMessages={arr.map((x) => x.msg)} toolCall={msg.toolCall} />
-                })}
-            {props.footer || null}
+            {props.renderContent(() => {
+                lastShowContentTimestampRef.current = Date.now()
+                scrollToBottomIfNeeded()
+            })}
         </div>
     )
+}
+
+export function ChatUi<T extends readonly ToolDefinition[] = readonly ToolDefinition[]>(
+    props: {
+        messages: ChatMessage<T>[]
+        UserMessageComponent: React.ComponentType<{ messageIndex: number; groupedMessages: ChatMessage<T>[]; content: ChatMessageContent }>
+        AssistantMessageComponent: React.ComponentType<{
+            messageIndex: number
+            groupedMessages: ChatMessage<T>[]
+            text: string
+            onContentShow: (text: string) => void
+        }>
+        scrollContainer?: () => HTMLElement
+        ToolCallComponent: React.ComponentType<{ messageIndex: number; groupedMessages: ChatMessage<T>[]; toolCall: ToolCall<T> }>
+        footer?: React.ReactNode
+    } & ComponentProps<'div'>
+) {
+    const { messages, UserMessageComponent, AssistantMessageComponent, ToolCallComponent, footer, ...rest } = props
+
+    const groupedMessages = useMemo(() => groupMessages(props.messages), [props.messages])
+
+    const renderContent = useCallback(
+        (onExpanded: () => void) => {
+            return (
+                <>
+                    {groupedMessages
+                        .filter((msg) => msg.msg.role !== 'system' && msg.msg.role !== 'toolResponse')
+                        .map(({ indexes, msg }: { indexes: number[]; msg: Exclude<ChatMessage<T>, { role: 'system' | 'toolResponse' }> }, i, arr) => {
+                            const key = indexes[0]
+
+                            if (msg.role === 'user') {
+                                return (
+                                    <React.Fragment key={key}>
+                                        {msg.content.map((x, i2) => (
+                                            <props.UserMessageComponent key={i2} messageIndex={i} groupedMessages={arr.map((x) => x.msg)} content={x} />
+                                        ))}
+                                    </React.Fragment>
+                                )
+                            }
+                            if (msg.role === 'assistant') {
+                                return msg.content === '' ? null : (
+                                    <props.AssistantMessageComponent
+                                        key={key}
+                                        messageIndex={i}
+                                        groupedMessages={arr.map((x) => x.msg)}
+                                        text={msg.content}
+                                        onContentShow={() => {
+                                            onExpanded()
+                                        }}
+                                    />
+                                )
+                            }
+                            return <props.ToolCallComponent key={key} messageIndex={i} groupedMessages={arr.map((x) => x.msg)} toolCall={msg.toolCall} />
+                        })}
+                    {props.footer || null}
+                </>
+            )
+        },
+        [groupedMessages]
+    )
+
+    return <ScrollBox renderContent={renderContent} {...rest} />
 }
